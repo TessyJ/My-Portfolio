c4362a39fa448b42bc8b216eda0402f28b5d7292
Theresa <theresaolusoga@Bolajis-MacBook-Pro.local>
2023-07-02 16:43:32 -0400

    complted my portfolio

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import React from "react";
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Header from "./components/Main/Header";
+import Footer from "./components/Main/Footer";
+import About from "./components/About";
+import Portfolio from "./components/Portfolio";
+import Contact from "./components/Contact";
+import Resume from "./components/Resume";
+
+function App() {
+  return (
+    <div className="appContainer">
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<About />} />
+          <Route path="/portfolio" element={<Portfolio />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/resume" element={<Resume />} />
+        </Routes>
+        <Footer />
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
